5261cba8f3aec9d11d8b6f5acb788a0762038b35
Muh Royan Fauzi Maulana <royanfauzimaulana25@gmail.com>
2024-07-03 21:34:50 +0700

    Update payment.js

--- src/script/view/js/payment.js
+++ src/script/view/js/payment.js
@@ -37,7 +37,7 @@ const payment = () => {
             console.log(response);
             ModalSucces();
             await setTimeout(() => {
-                window.location.href = response;
+                window.location.href = response.invoice_url;
             },3000); 
             
         } catch (error) {
